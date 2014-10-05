8af1a39e6b306eaf9ecefe45ea899ca0c1708348
eugenekang <eugene.yc.kang@gmail.com>
2014-10-04 21:17:26 -0400

    Wrote the dictionary constructor into a dictionary literal for PEP8 style editor. Added a docstring

--- exercise3.py
+++ exercise3.py
@@ -8,6 +8,7 @@ __license__ = "MIT License"
 
 __status__ = "Prototype"
 
+
 def decide_rps(player1, player2):
     """
     Determines who wins the rock paper scissors game
@@ -22,9 +23,13 @@ def decide_rps(player1, player2):
         TypeError if input is not string
         ValueError if string is not Rock or Paper or Scissors
     """
+    #Check if the correct type is passed (string)
     if type(player1) is str and type(player2) is str:
+        #Set all input to uppercase and ensure the correct values are passed (ROCK, PAPER, or SCISSORS) for p1+2
         if player1.upper() == "ROCK" or player1.upper() == "PAPER" or player1.upper() == "SCISSORS":
             if player2.upper() == "ROCK" or player2.upper() == "PAPER" or player2.upper() == "SCISSORS":
+                #Returns the result of the rps_Dictionary with the input passed from player 1 and 2 as
+                #the keys for the tuple.
                 return rps_Dictionary[(player1.upper(), player2.upper())]
             else:
                 raise ValueError("Invalid Value for Player 2. Please enter ROCK, PAPER, or SCISSORS")
@@ -34,18 +39,22 @@ def decide_rps(player1, player2):
         raise TypeError("Invalid type. Please enter a string.")
 
 #Defining the Rock, Paper, Scissors "scoring" dictionary
-rps_Dictionary = {}
+    """
+    Dictionary storing the different possible outcomes and "scores" for the Rock, Paper, Scissors game.
+    Different combinations are stored as a tuple in the compound-key format. (player1, player2)=return_value
+    """
+rps_Dictionary = {
+    ("SCISSORS", "SCISSORS"): 0,
+    ("ROCK", "ROCK"): 0,
+    ("PAPER", "PAPER"): 0,
+    ("SCISSORS", "PAPER"): 1,
+    ("PAPER", "ROCK"): 1,
+    ("ROCK", "SCISSORS"): 1,
+    ("SCISSORS", "ROCK"): 2,
+    ("ROCK", "PAPER"): 2,
+    ("PAPER", "SCISSORS"): 2
+}
 
-rps_Dictionary[("SCISSORS", "SCISSORS")] = 0
-rps_Dictionary[("ROCK", "ROCK")] = 0
-rps_Dictionary[("PAPER", "PAPER")] = 0
 
-rps_Dictionary[("SCISSORS", "PAPER")] = 1
-rps_Dictionary[("PAPER", "ROCK")] = 1
-rps_Dictionary[("ROCK", "SCISSORS")] = 1
-
-rps_Dictionary[("SCISSORS", "ROCK")] = 2
-rps_Dictionary[("PAPER", "SCISSORS")] = 2
-rps_Dictionary[("ROCK", "PAPER")] = 2
 
 
